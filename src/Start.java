20255a4f238cb1d93ecbd193d239b096468073d4
nileshberaNB <nileshbera3012@gmail.com>
2022-07-11 16:26:40 +0530

    final commit(completed)

--- src/Start.java
+++ src/Start.java
@@ -6,6 +6,7 @@ import com.student.manage.Student;
 import com.student.manage.StudentDao;
 public class Start {
 
+
 	public static void main(String[] args) throws IOException {
 		System.out.println("Welcome Student");
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -13,7 +14,8 @@ public class Start {
 			System.out.println("Press 1 to add student");
 			System.out.println("Press 2 to delete student");
 			System.out.println("Press 3 to display student");
-			System.out.println("Press 4 to exit app");
+			System.out.println("Press 4 to update student");
+			System.out.println("Press 5 to exit app");
 			int c = Integer.parseInt(br.readLine());
 
 			if(c==1)
@@ -54,6 +56,16 @@ public class Start {
 			{
 				StudentDao.showAllStudent();
 			}else if(c==4)
+			{
+				boolean a=StudentDao.updateStudent();
+				if(a)
+				{
+					System.out.println("Updated successfully...");
+				}else
+				{
+					System.out.println("Something went wrong try again");
+				}
+			}else if(c==5)
 			{
 
 				break;
--- src/com/student/manage/StudentDao.java
+++ src/com/student/manage/StudentDao.java
@@ -1,7 +1,7 @@
 package com.student.manage;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Connection;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -53,21 +53,21 @@ public class StudentDao {
             Statement stmt = con.createStatement();
             
 
-            ResultSet set=stmt.executeQuery(q);
+            ResultSet set = stmt.executeQuery(q);
             while(set.next())
             {
                 int id=set.getInt(1);
                 
                 String name=set.getString(2);
                 
-                String phone=set.getString(3);
+                String phone=set.getString("sphone");
                 
                 String city=set.getString("scity");
 
                 System.out.println("ID : "+ id);
                 System.out.println("Name : "+ name);
                 System.out.println("Phone : "+ phone);
-                System.out.println("ID : "+ city);
+                System.out.println("City : "+ city);
                 System.out.println("===============================");
             }
         }catch(Exception e){
@@ -75,5 +75,63 @@ public class StudentDao {
         }
         return f;
     }
+
+    public static boolean updateStudent() {
+        boolean f=false;
+            try{
+    
+                Connection con = CP.createC();
+                
+                String q="update students set sname=?, sphone=?, scity=? where sid=?";
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+                System.out.print("name:"+" ");
+                String name=br.readLine();
+
+                System.out.print("phone:"+" ");
+				String phone=br.readLine();
+
+                System.out.print("city:"+" ");
+				String city=br.readLine();
+
+				System.out.print("enter sid:"+" ");
+                int userID=Integer.parseInt(br.readLine());
+               
+                PreparedStatement pstmt = con.prepareStatement(q);
+
+                pstmt.setString(1, name);
+                pstmt.setString(2, phone);
+                pstmt.setString(3, city);
+                pstmt.setInt(4, userID);
+
+                pstmt.executeUpdate();
+                f=true;
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return f;
+    }
+
+    // public static boolean updateStudent(Student st) {
+    //     boolean f=false;
+    //     try{
+
+    //         Connection con = CP.createC();
+    //         String q="alter table students modify sname=?, sphone=?, scity=? where sid=?)";
+            
+           
+    //         PreparedStatement pstmt = con.prepareStatement(q);
+    //         pstmt.setString(1, st.getStudentName());
+    //         pstmt.setString(2, st.getStudentPhone());
+    //         pstmt.setString(3, st.getStudentCity());
+    //         pstmt.setInt(4, st.getStudentID());
+    //         pstmt.executeUpdate();
+    //         f=true;
+    //     }catch(Exception e){
+    //         e.printStackTrace();
+    //     }
+    //     return f;
+    // }
 
 }
